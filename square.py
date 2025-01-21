c77e92c483a76cb8095fcb7c18ab01eed79e24ce
Taha Eren Özden <tahaerenozden@gmail.com>
2025-01-21 21:14:21 +0300

    only one(1) turtle used for square.

---
+++ square.py
@@ -0,0 +1,16 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Square Python Turtle")
+# only one(1) turtle used for square
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+
+turtle.done()
